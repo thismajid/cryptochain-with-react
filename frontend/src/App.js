07cc5ac8a439c8fc833cdc7e9ed745730b5657f9
thismajid <mkhorshidian72@gmail.com>
2022-02-11 18:14:29 +0330

    add react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import Blocks from "./components/Blocks/Blocks";
 import wallet from "./assets/wallet.png";
 import { FaKey, FaCoins } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function App() {
   const [walletInfo, setWalletInfo] = useState({ address: "", balance: "" });
@@ -24,13 +25,16 @@ function App() {
         </div>
         <div>
           <h1>Your Wallet</h1>
+          <Link to="transact">
+            <button>Send Money</button>
+          </Link>
           <div className="address">
             <FaKey className="icon" />
             Address: {walletInfo.address}
           </div>
           <div className="balance">
             <FaCoins className="icon" />
-            Balance: {walletInfo.address}
+            Balance: {walletInfo.balance}
           </div>
         </div>
       </div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import ConductTransaction from "./components/ConductTransaction/ConductTransaction";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <ConductTransaction />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/transact" component={ConductTransaction} />
+      <Route path="/" exact component={App} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById("root")
 );
