5aafd1b636d5f854d19d7ad74cbadfa2aac788b7
thismajid <mkhorshidian72@gmail.com>
2022-02-11 12:10:37 +0330

    add data detail component

--- frontend/src/components/Blocks/Block/Block.js
+++ frontend/src/components/Blocks/Block/Block.js
@@ -1,10 +1,22 @@
 import { useState } from "react";
+import Transaction from "./Transaction/Transaction";
 
 const Block = ({ block }) => {
   const [displayTransaction, setDisplayTransaction] = useState(false);
 
   const ToggleTransaction = () => {
     setDisplayTransaction(!displayTransaction);
+  };
+
+  const showTransactions = (transactions) => {
+    return transactions.map((transaction) => {
+      return (
+        <div key={transaction.id}>
+          <hr />
+          <Transaction transaction={transaction} />
+        </div>
+      );
+    });
   };
 
   const { timestamp, hash, data } = block;
@@ -20,7 +32,12 @@ const Block = ({ block }) => {
       <div>Hash: {hashDisplay}</div>
       <div>Timestamp: {new Date(timestamp).toLocaleString()}</div>
       <div onClick={ToggleTransaction} style={{ cursor: "pointer" }}>
-        Data: {displayTransaction ? JSON.stringify(data) : dataDisplay}
+        Data:{" "}
+        {displayTransaction ? (
+          showTransactions(data)
+        ) : (
+          <span className="show">Show Transaction</span>
+        )}
       </div>
     </div>
   );
