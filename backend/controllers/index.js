44d071ec1bcb951ea1c1a7c47ce57a8f72c005bd
thismajid <mkhorshidian72@gmail.com>
2022-02-11 11:01:49 +0330

    add some transactions

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -76,6 +76,55 @@ const walletInfo = (req, res) => {
   });
 };
 
+const walletFoo = new Wallet();
+const walletBar = new Wallet();
+
+const generateWalletTransaction = ({ wallet, recipient, amount }) => {
+  const transaction = wallet.createTransaction({
+    recipient,
+    amount,
+    chain: blockchain.chain,
+  });
+
+  transactionPool.setTransaction(transaction);
+};
+
+const walletAction = () =>
+  generateWalletTransaction({
+    wallet,
+    recipient: walletFoo.publicKey,
+    amount: 5,
+  });
+
+const walletFooAction = () =>
+  generateWalletTransaction({
+    wallet: walletFoo,
+    recipient: walletBar.publicKey,
+    amount: 10,
+  });
+
+const walletBarAction = () =>
+  generateWalletTransaction({
+    wallet: walletBar,
+    recipient: wallet.publicKey,
+    amount: 15,
+  });
+
+for (let index = 0; index < 10; index++) {
+  if (index % 3 === 0) {
+    walletAction();
+    walletFooAction();
+  } else if (index % 3 === 1) {
+    walletAction();
+    walletBarAction();
+  } else {
+    walletFooAction();
+    walletBarAction();
+  }
+
+  transactionMiner.mineTransactions();
+}
+
 module.exports = {
   syncOnConnect,
   getBlocks,
