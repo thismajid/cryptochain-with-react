7446e4839d5301ace484aceb00e3ae50fd0d50e5
thismajid <mkhorshidian72@gmail.com>
2022-02-11 11:58:37 +0330

    show complete block data

--- frontend/src/components/Blocks/Block/Block.js
+++ frontend/src/components/Blocks/Block/Block.js
@@ -1,18 +1,27 @@
+import { useState } from "react";
+
 const Block = ({ block }) => {
+  const [displayTransaction, setDisplayTransaction] = useState(false);
+
+  const ToggleTransaction = () => {
+    setDisplayTransaction(!displayTransaction);
+  };
+
   const { timestamp, hash, data } = block;
   const hashDisplay = `${hash.substring(0, 15)}...`;
   const stringifiedData = JSON.stringify(data);
   const dataDisplay =
     stringifiedData.length > 35
-      ? `${stringifiedData.substring(0, 15)}...`
+      ? `${stringifiedData.substring(0, 35)}...`
       : stringifiedData;
 
   return (
     <div className="block">
-      {console.log(timestamp)}
       <div>Hash: {hashDisplay}</div>
       <div>Timestamp: {new Date(timestamp).toLocaleString()}</div>
-      <div>Data: {dataDisplay}</div>
+      <div onClick={ToggleTransaction} style={{ cursor: "pointer" }}>
+        Data: {displayTransaction ? JSON.stringify(data) : dataDisplay}
+      </div>
     </div>
   );
 };
